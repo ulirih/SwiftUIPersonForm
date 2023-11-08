75387431d9a9fa52d75cb3219d87347839f92259
Andrei Perevedniuk <perevedniuk@gmail.com>
2023-11-08 19:57:30 +0200

    Initial Commit

---
+++ SwiftUIPersonForm/SwiftUIPersonFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPersonFormApp.swift
+//  SwiftUIPersonForm
+//
+//  Created by andrey perevedniuk on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPersonFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
